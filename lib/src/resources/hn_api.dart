c196a1118b6e3b696b2aa84d99c6ae2ef527fecf
craques <cmuzhanye@gmail.com>
2022-12-30 12:22:35 +0200

    initial commit

---
+++ lib/src/resources/hn_api.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' show Client;
+
+class HNClient {
+  final client = Client();
+  final root = 'https://hacker-news.firebaseio.com/v0';
+
+  getItemIds() async {
+    return await client.get(Uri.parse('$root/topstories.json'));
+  }
+
+  getItem(int id) async {
+    return await client.get(Uri.parse('$root/item/$id.json');
+  }
+}
