b917f34b0816a03924e3211e929cb75d174e7a83
craques <cmuzhanye@gmail.com>
2023-01-01 12:56:31 +0200

    Added get ids test

--- lib/src/resources/api/hn_api.dart
+++ lib/src/resources/api/hn_api.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'package:hn_app/src/resources/api/hn_api_types.dart';
 import 'package:http/http.dart' show Client;
 
-class HNClient {
-  final client = Client();
+class HNAPI {
+  Client client = Client();
   final root = 'https://hacker-news.firebaseio.com/v0';
 
   getItemIds() async {
--- lib/src/resources/api/hn_api_test.dart
+++ lib/src/resources/api/hn_api_test.dart
@@ -1,9 +1,21 @@
+import 'dart:convert';
+
+import 'package:hn_app/src/resources/api/hn_api.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('HN API', () {
-    test('should run without errors', () {
-      expect(3, 3);
+    test('should return an array of ids when requested', () async {
+      final api = HNAPI();
+      api.client = MockClient((Request request) async {
+        return Response(jsonEncode([1, 2, 3, 4]), 200);
+      });
+
+      final ids = await api.getItemIds();
+
+      expect(ids, [1, 2, 3, 4]);
     });
   });
 }
